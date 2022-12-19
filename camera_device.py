2a65f9e6c58bc975cf148cd28d631e8e80a795e3
markcalendario <markcalendario@gmail.com>
2022-12-19 11:26:55 +0800

    add comments

--- camera_device.py
+++ camera_device.py
@@ -2,12 +2,15 @@ import cv2
 
 def get_available_camera():
 
+  # Allow loop to test camera indices from -50 to 100
   for camera_index in range(-50, 101):
     camera = cv2.VideoCapture(camera_index)
     is_cam_capturing, _ = camera.read()
     camera.release()
 
+    # If camera is available then return the index
     if is_cam_capturing:
       return camera_index
   
+  # If there are no camera available, return None
   return None
